c52ca2c06385ef154ca6cb1521e749e2f558d863
Kirti Singh <121571038+Kirti0513@users.noreply.github.com>
2024-01-05 13:15:04 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -4,7 +4,24 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+         stack = []
+        bracket_mapping = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in bracket_mapping.values():
+                stack.append(char)
+            elif char in bracket_mapping.keys():
+                if stack == [] or bracket_mapping[char] != stack.pop():
+                    return False
+            else:
+                return False
+
+        return stack == []
+
+test_case = input("s = ")
+sol = Solution()
+result = sol.isValid(test_case)
+print(f"{'true' if result else 'false'}")
     
 
 
