e2627c5f462bcfeef4f48b4b642ca8bffa6447b6
Kirti Singh <121571038+Kirti0513@users.noreply.github.com>
2024-01-05 13:29:56 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -4,4 +4,29 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        roman_to_int_mapping = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        result = 0
+        prev_value = 0
+        for char in reversed(s):
+            current_value = roman_to_int_mapping[char]
+            if current_value < prev_value:
+                result -= current_value
+            else:
+                result += current_value
+            prev_value = current_value
+        return result
+
+
+roman_numeral = input("s = ")
+sol = Solution()
+result = sol.romanToInt(roman_numeral)
+print(f"{result}")
